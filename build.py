2779ba7915bf0ea74edfc42217cabb3396aa83da
Alexander Nazimov <115407821+macht1212@users.noreply.github.com>
2025-08-31 23:23:20 +0300

    Update build.py

--- build.py
+++ build.py
@@ -1,14 +1,19 @@
+# build.py — poetry build hook
 from pathlib import Path
-from setuptools import Extension, setup
+from setuptools import Extension
 
 SRC = Path(__file__).parent / "src"
 
-ext_modules = [
-    Extension(
-        name="kvxdb_core",
-        sources=[str(SRC / "_kvxdb.c"), str(SRC / "kvxdb.c")],
-        include_dirs=[str(SRC)],
+def build(setup_kwargs: dict) -> None:
+    setup_kwargs.update(
+        dict(
+            ext_modules=[
+                Extension(
+                    name="kvxdb_core",
+                    sources=[str(SRC / "_kvxdb.c"), str(SRC / "kvxdb.c")],
+                    include_dirs=[str(SRC)],
+                )
+            ],
+            zip_safe=False,
+        )
     )
-]
-
-setup(ext_modules=ext_modules)
